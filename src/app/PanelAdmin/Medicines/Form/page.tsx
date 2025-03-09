34c48732620964a4a40c95a3abc2f20d45bcd912
Fabi Mrtnz <tldodnssa@gmail.com>
2025-03-09 00:29:37 -0400

    build: Fetching para Laboratorio
    Se crearon los archivos en types y services para hacer el fetching de datos en laboratorio

--- src/app/PanelAdmin/Medicines/Form/page.tsx
+++ src/app/PanelAdmin/Medicines/Form/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "@/components/Header";
+import Header from "@/components/header";
 import Footer from "@/components/Footer";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
